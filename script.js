a6fb2e112c0b968e1cbc8034e64ae1b9c71d63a4
vinayak500 <vinayakshetty500@gmail.com>
2022-08-03 13:33:40 +0530

    craeted intial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var outer_container = document.getElementById('outer-container');
+var innerbutton = document.getElementById('toggle-button-inner-container');
+var outerbutton = document.getElementById('toggle-button-outer-container');
+var heading = document.getElementById('heading');
+
+innerbutton.addEventListener('click' , function(){
+    innerbutton.style.marginLeft="40px";
+    innerbutton.style.backgroundColor='black';
+
+    outerbutton.style.backgroundColor='white';
+    heading.style.color='white';
+    outer_container.style.backgroundColor='black';
+});
